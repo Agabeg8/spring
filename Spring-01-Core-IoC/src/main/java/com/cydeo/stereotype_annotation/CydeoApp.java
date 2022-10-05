8b0941594941b3f33088ce9984efd31dced74590
Agabeg8 <agabegh@gmail.com>
2022-10-05 11:11:14 +0200

    java class updated

--- Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/CydeoApp.java
@@ -7,6 +7,7 @@ public class CydeoApp {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCourse.class);
+        context.getBean(Java.class).getTeachingHours();
 
 
     }
