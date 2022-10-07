bfd51ab6fb114aa21f406fa17a8979bd24dc0ece
Agabeg8 <agabegh@gmail.com>
2022-10-07 12:59:45 +0200

    Added src folder

---
+++ Spring-04-Abstaction/src/main/java/com/cydeo/model/Comment.java
@@ -0,0 +1,9 @@
+package com.cydeo.model;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    private String author;
+    private String text;
+}
---
+++ Spring-04-Abstaction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,14 @@
+package com.cydeo.proxy;
+
+import com.cydeo.model.Comment;
+
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+
+        System.out.println("Sending notification for comment: " + comment.getText());
+
+    }
+
+
+}
