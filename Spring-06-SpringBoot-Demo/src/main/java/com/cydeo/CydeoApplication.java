85bc9670e090b79c67763dfc51444a2fd1b653a0
Agabeg8 <agabegh@gmail.com>
2022-10-12 09:33:25 +0200

    pom

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CydeoApplication.class, args);
+    }
+
+}
