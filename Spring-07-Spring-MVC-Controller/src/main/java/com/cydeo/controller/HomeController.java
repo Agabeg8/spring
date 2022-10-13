b80283a1d954b5288e004d5be503a7e4b96d6f79
Agabeg8 <agabegh@gmail.com>
2022-10-13 09:58:24 +0200

    Spring MVC - Controller

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home.html";
+    }
+
+    @RequestMapping("/welcome")
+    public String home2(){
+        return "welcome.html";
+    }
+}
