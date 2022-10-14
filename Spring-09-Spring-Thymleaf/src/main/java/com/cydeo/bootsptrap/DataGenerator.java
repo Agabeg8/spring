6fab214aee7f623911752bc03db9e94f00ccfa47
Agabeg8 <agabegh@gmail.com>
2022-10-14 17:46:02 +0200

    Spring Thymleaf

---
+++ Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/bootsptrap/DataGenerator.java
@@ -0,0 +1,22 @@
+package com.cydeo.bootsptrap;
+
+import com.cydeo.model.Student;
+import com.github.javafaker.Faker;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student> createStudent(){
+
+        List<Student> students = Arrays.asList(
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50), new Faker().address().state())
+                );
+        return students;
+    }
+}
