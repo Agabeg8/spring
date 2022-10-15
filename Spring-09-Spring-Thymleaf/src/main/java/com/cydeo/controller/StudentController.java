c470df4f54859ae20d2156a8ff63332edc1fce83
Agabeg8 <agabegh@gmail.com>
2022-10-15 11:31:29 +0200

    Spring Thymeleaf

---
+++ Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootsptrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+}
