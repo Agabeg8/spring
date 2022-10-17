86531093ebd538134138a7a6c5c3947ace5d88d3
Agabeg8 <agabegh@gmail.com>
2022-10-17 10:18:49 +0200

    Spring Thymeleaf

--- Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/Spring09SpringThymeleafApplication.java
+++ Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/Spring09SpringThymeleafApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring09SpringThymleafApplication {
+public class Spring09SpringThymeleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring09SpringThymleafApplication.class, args);
+		SpringApplication.run(Spring09SpringThymeleafApplication.class, args);
 	}
 
 }
