d71fba780dfac9dfb8e338cc8b3db91c8fa39f73
Agabeg8 <agabegh@gmail.com>
2022-10-21 16:31:56 +0200

    Spring Thymeleaf

--- Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,17 +1,26 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
 
-    @GetMapping("register")
-    public String showForm(){
+    @GetMapping("/register")
+    public String showForm(Model model){
 
-        return "mentor/mentor-register";
+        model.addAttribute(new Mentor());
+
+        List<String> batchList = Arrays.asList("JD1","JD2","EU1","EU2","B18","B20");
+        model.addAttribute("batchList", batchList);
+        return "/mentor/mentor-register";
     }
 }
