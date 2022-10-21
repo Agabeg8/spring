bda95f4d03af305678e0bb4fbec8a527cc443128
Agabeg8 <agabegh@gmail.com>
2022-10-21 16:58:59 +0200

    Spring Thymeleaf Post and redirect methods

--- Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -23,4 +25,14 @@ public class MentorController {
         model.addAttribute("batchList", batchList);
         return "/mentor/mentor-register";
     }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor){
+
+
+
+        //return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
+    }
+
 }
