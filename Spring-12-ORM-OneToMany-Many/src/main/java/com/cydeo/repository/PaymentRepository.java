97c298438b99719be4e2b71470059320463c1dc2
Agabeg8 <agabegh@gmail.com>
2022-11-08 15:43:17 +0100

    Cinema Lab

--- Spring-12-ORM-OneToMany-Many/src/main/java/com/cydeo/repository/PaymentRepository.java
+++ Spring-12-ORM-OneToMany-Many/src/main/java/com/cydeo/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.cydeo.repository;
 import com.cydeo.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
 }
