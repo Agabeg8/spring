31321c789dd9abd7bbb543ad0de01919cfb230f9
Agabeg8 <agabegh@gmail.com>
2022-11-08 10:41:41 +0100

    Cinema Lab

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/Spring13OrmCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13OrmCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13OrmCinemaLabApplication.class, args);
+    }
+
+}
---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Movie extends BaseEntity{
+
+    private String name;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+    private BigDecimal price;
+
+
+}
