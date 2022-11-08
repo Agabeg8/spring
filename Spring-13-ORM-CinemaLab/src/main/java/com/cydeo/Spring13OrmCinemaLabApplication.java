bf25d2f75f93a1aed57d0b2b659fd4639fc009fe
Agabeg8 <agabegh@gmail.com>
2022-11-08 15:31:23 +0100

    Cinema Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/Spring13OrmCinemaLabApplication.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/Spring13OrmCinemaLabApplication.java
@@ -1,13 +1,24 @@
 package com.cydeo;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Spring13OrmCinemaLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring13OrmCinemaLabApplication.class, args);
+    }
+
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+
     }
 
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/User.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/User.java
@@ -17,7 +17,9 @@ public class User extends BaseEntity{
     private String username;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account_detials_id")
+    @JoinColumn(name = "account_details_id")
     private Account account;
+
+
 
 }
