8f4666f19ba30dbf6ae2002f42cb672e96b4b91f
Agabeg8 <agabegh@gmail.com>
2022-11-08 15:07:24 +0100

    Cinema Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Account.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Account.java
@@ -1,12 +1,30 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.UserRole;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Data
-public class Account {
+@Table(name = "account_details")
+public class Account extends BaseEntity{
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @OneToOne(mappedBy = "account")
+    private User user;
+
+
 }
