c48546f014b003665d4ae059e2aa02372c5ad8ee
Agabeg8 <agabegh@gmail.com>
2022-11-08 09:49:23 +0100

    Mapping

--- Spring-12-ORM-OneToMany-Many/src/main/java/com/cydeo/model/Item.java
+++ Spring-12-ORM-OneToMany-Many/src/main/java/com/cydeo/model/Item.java
@@ -1,4 +1,20 @@
 package com.cydeo.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
+@Data
+@NoArgsConstructor
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
 }
