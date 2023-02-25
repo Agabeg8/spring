14169179281f34522f6a30f26dbe4199a78ec138
agabeg8 <agabegh@gmail.com>
2023-02-25 21:45:56 +0100

    Rest consuming

---
+++ Spring-17-REST-ConsumingAips/src/main/java/com/cydeo/Spring17RestConsumingAipsApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17RestConsumingAipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingAipsApplication.class, args);
+    }
+
+}
