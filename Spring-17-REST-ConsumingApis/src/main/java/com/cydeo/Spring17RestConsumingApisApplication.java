68466e130ea2c46c569f6905845b2215c9b0d854
agabeg8 <agabegh@gmail.com>
2023-02-26 22:11:39 +0100

    Rest Consuming

---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Spring17RestConsumingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -0,0 +1,32 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class Consume_RestTemplate {
+
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    private final RestTemplate restTemplate;
+
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    @GetMapping
+    public User[] readAllUsers(){
+
+       ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI,User[].class);
+
+
+       return responseEntity.getBody();
+
+    }
+}
