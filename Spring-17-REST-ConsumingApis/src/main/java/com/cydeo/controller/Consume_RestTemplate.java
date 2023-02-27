64171f074b7031d95efcc816a0390b1448d2d8e5
agabeg8 <agabegh@gmail.com>
2023-02-27 21:53:27 +0100

    Rest Test Api

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,13 +1,14 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -33,4 +34,19 @@ public class Consume_RestTemplate {
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL,Object.class, id);
     }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String>entity = new HttpEntity<>(headers);
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET, entity,Object.class);
+
+        return response;
+
+    }
+
 }
