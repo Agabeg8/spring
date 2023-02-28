840a7934f4ded47bc0923a0dcd420186c83a72cf
agabeg8 <agabegh@gmail.com>
2023-02-28 10:53:47 +0100

    Feing client

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_FeignClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_FeignClient.java
@@ -1,4 +1,24 @@
 package com.cydeo.controller;
 
+import com.cydeo.client.UserClient;
+import com.cydeo.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Consume_FeignClient {
+
+    private final UserClient userClient;
+
+    public Consume_FeignClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers(){
+
+        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved",userClient.getUsers()));
+    }
+
 }
---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,31 @@
+package com.cydeo.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseWrapper {
+
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.data = data;
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+}
