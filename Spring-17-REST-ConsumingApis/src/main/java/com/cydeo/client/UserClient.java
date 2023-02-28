89dc8ffe35c216ee654588253188d2e48de6cba7
agabeg8 <agabegh@gmail.com>
2023-02-28 10:41:07 +0100

    Feing client

---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/client/UserClient.java
@@ -0,0 +1,22 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com/", name = "USER-CLIENT")
+public interface UserClient {
+    @GetMapping("/users")
+    List<User> getUsers();
+
+
+
+
+
+}
+
+
+
+
