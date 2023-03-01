38b08d31faa6ca6749af7d472fcf24466eb1a4b8
agabeg8 <agabegh@gmail.com>
2023-03-01 13:24:42 +0100

    Web Client

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -1,4 +1,16 @@
 package com.cydeo.controller;
 
+import com.cydeo.repository.GenreRepository;
+import com.cydeo.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Consume_WebClient {
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final GenreRepository genreRepository;
+
+    public Consume_WebClient(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
 }
