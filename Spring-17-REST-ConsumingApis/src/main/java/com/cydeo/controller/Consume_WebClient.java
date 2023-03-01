9ef4ac6e517610bd39011d781cd8a89c940ecf78
agabeg8 <agabegh@gmail.com>
2023-03-01 13:28:01 +0100

    Web Client

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -1,8 +1,11 @@
 package com.cydeo.controller;
 
+import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class Consume_WebClient {
@@ -13,4 +16,17 @@ public class Consume_WebClient {
         this.movieCinemaRepository = movieCinemaRepository;
         this.genreRepository = genreRepository;
     }
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+
+
+
+
+
+
+
+
 }
